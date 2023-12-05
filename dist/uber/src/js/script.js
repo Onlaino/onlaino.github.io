04f5112247feffb6e37d33cd85b4dcd915bdb01c
Oleg Vasilev <v.oleg2908@gmail.com>
2023-12-05 20:45:15 +0300

    50% complete hero section

---
+++ dist/uber/src/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__item');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    })
+})
+
